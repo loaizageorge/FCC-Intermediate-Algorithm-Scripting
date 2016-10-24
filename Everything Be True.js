adaa94312dd9e76e409775e280d0472ea3afd14e
George <loaizageorge@gmail.com>
2016-10-24 17:40:50 -0400

    Update and rename Everything Be True to Everything Be True.js

--- Everything Be True.js
+++ Everything Be True.js
@@ -2,11 +2,14 @@ function truthCheck(collection, pre) {
   // Is everyone being true?
  var countTrue = 0;
   for (var i = 0; i < collection.length; i++){
+  // Check if the item has the property as declared in the second argument.
+  // Also it needs to have a real value in it, can't have falsy values in it.
  	if((collection[i].hasOwnProperty(pre) && Boolean(collection[i][pre])))  {
   	countTrue ++;
   }
- // else{console.log(collection[i][pre]);} 
+ 
  }
+ // If all items in the collection are true, return true
   return collection.length == countTrue;
 }
 
